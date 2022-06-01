456a1d35f04fc279d036121ca2918447ba6372e4
khaddyBola <khadijahmusa232@gmail.com>
2022-06-01 18:53:44 +0100

    First Commit

--- Assignment-1/script.js
+++ Assignment-1/script.js
@@ -7,31 +7,67 @@ class Car {
     }
 
     calcAccel() {
-        return this.speed + 10;
-    }
-}
-
-const car1 = new Car('BMW', 120)
-const car2 = new Car('Mercedes', 95)
-
-console.log(`your car is running at ${car1.calcAccel()} km/h`);
-console.log(`Your car is running at ${car2.calcAccel()} km/h`);
-
-
-class Car2 {
-    constructor(make, speed) {
-        this.make = make;
-        this.speed = speed;
+     this.speed += 10;
+     console.log(`${this.make} is going at ${this.speed} km/h`)
     }
 
     calcBrake() {
-        return this.speed - 5;
+        this.speed -= 5;
+        console.log(`${this.make} is going at ${this.speed} km/h`)
     }
 }
 
-const car_1 = new Car2('BMW', 120)
-const car_2 = new Car2('Mercedes', 95)
 
-console.log(`Your car is running at ${car_1.calcBrake()} km/h`);
-console.log(`Your car is running at ${car_2.calcBrake()} km/h`);
+const car1 = new Car('BMW', 120)
+car1.calcAccel()
+car1.calcAccel()
+car1.calcBrake()
+car1.calcBrake()
+car1.calcAccel()
+car1.calcAccel()
+car1.calcBrake()
+car1.calcBrake()
+
+
+// 1. Create Class for Account
+// 2. 3_params: owner, pin, movemets
+// 3. 3_Methods
+// Create username
+// Deposit
+// Withdrawer
+
+// SOLUTION
+
+class Account {
+    constructor(owner, pin, movements) {
+        this.owner = owner;
+        this.pin = pin;
+        this.movements = movements;
+    }
+
+    userName() {
+        
+    }
+
+    deposit() {
+        this.movements += 10;
+        console.log(`${this.owner} is at ${this.movements}`)
+
+    }
+
+    withdrawer() {
+        this.movements -= 5;
+        console.log(`${this.owner} is at ${this.movements}`)
+    }
+}
+
+const accOwner = new Account('Jessica Davis', 4444, 150);
+accOwner.deposit()
+accOwner.deposit()
+accOwner.withdrawer()
+accOwner.deposit()
+
+
+
+
 
